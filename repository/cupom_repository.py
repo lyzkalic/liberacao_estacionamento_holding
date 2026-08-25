abecd2e0d1833cb3cdae6a6c2f287d4defd4ace3
✮ ⋆ Lyz Kalic  ⋆ ✮ <kaliclyz@gmail.com>
2026-08-25 16:03:59 -0300

    Add CUPOM_ID constant to CupomRepository class

--- repository/cupom_repository.py
+++ repository/cupom_repository.py
@@ -2,6 +2,8 @@ import requests
 
 class CupomRepository:
 
+CUPOM_ID = "150e6802-54e3-49a5-b2eb-a1357327e10c"
+
     def __init__(self):
         self.base_url = (
             "https://conectahub-internal.sacavalcante.com.br"
