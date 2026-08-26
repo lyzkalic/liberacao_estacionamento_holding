4a7f4fbcac8f2babe9dbedb067df174c9254d180
✮ ⋆ Lyz Kalic  ⋆ ✮ <kaliclyz@gmail.com>
2026-08-26 10:33:44 -0300

    Update autenticacao_service.py

--- services/autenticacao_service.py
+++ services/autenticacao_service.py
@@ -10,7 +10,7 @@ class AutenticacaoService:
         base_url
         or os.getenv(
             "CONECTA_HUB_URL",
-            "https://conectahub-internal.sacavalcante.com.br/api/v1",
+            "https://conectahub.sacavalcante.com.br/api/v1",
         ).rstrip("/")
     )
 
